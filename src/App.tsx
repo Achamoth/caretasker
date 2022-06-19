9489b79c359154eec64599936109eb110f1694cf
Ammar Abu Shamleh <ammarabushamleh@hotmail.com>
2022-06-19 20:19:20 +0800

    Layout and styling'

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProviderPage } from "./Provider/ProviderPage";
 import styles from "./App.module.css";
+import { TopBar } from "./TopBar";
+import { NavBar } from "./NavBar";
 
 function App() {
   return (
     <>
-      <div className={styles.header}>Caretasker</div>
-      <Router>
-        <Routes>
-          <Route path="providers/:providerName" element={<ProviderPage />} />
-        </Routes>
-      </Router>
+      <div className={styles.header}>
+        <TopBar />
+      </div>
+      <div className={styles.twoPanel}>
+        <div className={styles.navBar}>
+          <NavBar />
+        </div>
+        <div className={styles.content}>
+          <Router>
+            <Routes>
+              <Route
+                path="providers/:providerName"
+                element={<ProviderPage />}
+              />
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/TopBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./Images/THealth_logo.svg";
+import styles from "./TopBar.module.css";
+
+export function TopBar(): React.ReactElement {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <img src={logo} width={60} height={50} />
+      </div>
+      <div className={styles.text}>Caretasker</div>
+    </div>
+  );
+}
