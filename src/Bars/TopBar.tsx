27d6dc9e47765487ac508eff357a3667f7d40e4f
Ammar Abu Shamleh <ammarabushamleh@hotmail.com>
2022-06-23 14:18:20 +0800

    Logos

--- src/Bars/TopBar.tsx
+++ src/Bars/TopBar.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import logo from "../Images/THealth_logo.svg";
+import logo from "../Images/THealthLogo.png";
+import caretasker_logo from "../Images/Logo.png";
 import styles from "./TopBar.module.css";
 
 export function TopBar(): React.ReactElement {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img src={logo} width={60} height={50} alt="" />
+        <img src={logo} width={77} height={35} alt="" />
       </div>
-      <div className={styles.text}>Caretasker</div>
+      <div className={styles.caretaskerLogo}>
+        <img src={caretasker_logo} width={168} height={40} alt="" />
+      </div>
     </div>
   );
 }
